76daf0ab7f836a7b637fa8ab394ce3e99fa129b0
Sathish Kumar <sathishweb27@gmail.com>
2021-11-02 19:38:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import * as mui from '@mui/material'
+import * as Icons from '@mui/icons-material'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="price-table">
+          <div className="title">
+            <h2>Free</h2>
+            <b>$0/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p>Single User</p></div>
+            <div className="line"> <Icons.Done /><p>5GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Unlimited Private Projects</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Dedicated Phone Support</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Free Subdomain</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}} color="primary" variant="contained">Buy</mui.Button>  
+        </div>
+        <div className="price-table">
+        <div className="title" style={{backgroundColor:'#ffeebb'}}>
+            <h2>Plus</h2>
+            <b>$9/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p>5 User</p></div>
+            <div className="line"> <Icons.Done /><p>50GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Private Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Dedicated Phone Support</p></div>
+            <div className="line"> <Icons.Done /><p>Free Subdomain</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}} color="primary" variant="contained">Buy</mui.Button>
+        </div>
+        <div className="price-table">
+        <div className="title" style={{backgroundColor:'gold'}}>
+            <h2>Pro</h2>
+            <b>$49/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p><b>Unlimited User</b></p></div>
+            <div className="line"> <Icons.Done /><p>150GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Private Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Dedicated Phone Support</p></div>
+            <div className="line"> <Icons.Done /><p><b>Unlimited</b> Free Subdomain</p></div>
+            <div className="line"> <Icons.Done /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}} color="primary" variant="contained">Buy</mui.Button>
+        </div>
+      </div>
     </div>
   );
 }
